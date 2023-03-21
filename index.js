301a57e4f29454e2d866adc4a9c867f7ac82720f
arhamkhnz <md.arhamkhan09@gmail.com>
2023-03-21 14:34:32 +0530

    Added Multiple Effects

--- index.js
+++ index.js
@@ -1,14 +1,105 @@
-async function rotateAllText(word, characters, consoleElemId, delay) {
+const TypeWaveFlow = {
+  rotationPrint: async function (word, characters, consoleElemId, delay) {
     const consoleElem = document.getElementById(consoleElemId);
-    let current_word = '';
+    let current_word = "";
     let i = 0;
-  
+
     while (i < word.length) {
       const char = word[i];
       let characterFound = false;
+
+      for (let j = 0; j < characters.length; j++) {
+        consoleElem.innerText =
+          current_word + characters[j].repeat(word.length - i);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        if (char === characters[j] || char === characters[j].toUpperCase()) {
+          current_word = current_word + characters[j];
+          characterFound = true;
+          break;
+        }
+      }
+
+      if (!characterFound) {
+        current_word += char;
+      }
+
+      i += 1;
+    }
+
+    consoleElem.innerText = current_word;
+  },
+
+  scrollPrint: async function (
+    word,
+    characters,
+    consoleElemId,
+    delay,
+    completeLimit
+  ) {
+    const consoleElem = document.getElementById(consoleElemId);
+    let current_word = "";
+
+    for (let i = 0; i < word.length; i++) {
+      for (let j = 0; j < characters.length; j++) {
+        consoleElem.innerText += current_word + characters[j] + "\n";
+        consoleElem.scrollTop = consoleElem.scrollHeight;
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        if (
+          word[i] === characters[j] ||
+          word[i] === characters[j].toUpperCase()
+        ) {
+          current_word = current_word + characters[j];
+          if (current_word === word) {
+            for (let k = 0; k < completeLimit; k++) {
+              consoleElem.innerText += current_word + "\n";
+              consoleElem.scrollTop = consoleElem.scrollHeight;
+              await new Promise((resolve) => setTimeout(resolve, 20));
+            }
+          }
+          break;
+        }
+      }
+    }
+  },
+
+  smoothPrint: async function (word, characters, consoleElemId, delay) {
+    const consoleElem = document.getElementById(consoleElemId);
+    let current_word = "";
+
+    for (let i = 0; i < word.length; i++) {
+      let characterFound = false;
+      for (let j = 0; j < characters.length; j++) {
+        let output = current_word + characters[j];
+        consoleElem.innerText = output;
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        if (
+          word[i] === characters[j] ||
+          word[i] === characters[j].toUpperCase()
+        ) {
+          current_word += characters[j];
+          characterFound = true;
+          break;
+        }
+      }
+      if (!characterFound) {
+        current_word += word[i];
+      }
+    }
+    consoleElem.innerText = current_word;
+  },
+
+  afterEffect: async function(word, characters, consoleElemId, delay) {
+    const consoleElem = document.getElementById(consoleElemId);
+    let current_word = '';
+    const wordLength = word.length;
+  
+    for (let i = 0; i < wordLength; i++) {
+      let characterFound = false;
+      let char = word[i];
+      let wordSubstring = word.substring(i + 1);
   
       for (let j = 0; j < characters.length; j++) {
-        consoleElem.innerText = current_word + characters[j].repeat(word.length - i);
+        consoleElem.innerText = current_word + characters[j] + wordSubstring;
         await new Promise(resolve => setTimeout(resolve, delay));
         if (char === characters[j] || char === characters[j].toUpperCase()) {
           current_word = current_word + characters[j];
@@ -16,16 +107,12 @@ async function rotateAllText(word, characters, consoleElemId, delay) {
           break;
         }
       }
-  
       if (!characterFound) {
-        current_word += char;
+        current_word += word[i];
       }
-  
-      i += 1;
     }
-  
-    consoleElem.innerText = current_word;
   }
-  
-  module.exports = rotateAllText;
-  
+};
+
+// Export the TypeWaveFlow module
+module.exports = TypeWaveFlow;
